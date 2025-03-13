a8083d4851e766de695c5e02be2e86ab824d1a36
HawkingHuang <math0915@gmail.com>
2025-03-13 23:53:45 +0800

    install bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-    <div class="text-red-500">test</div>
   </header>
 
   <main>
-    <TheWelcome />
+    
   </main>
 </template>
 
