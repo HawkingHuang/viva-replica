cddcfa742362e97737117bf87fdc002aa8a73bae
HawkingHuang <math0915@gmail.com>
2025-03-14 00:40:38 +0800

    header layout

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="h-[46px] bg-[#3f3c42]">
+      <div class="container-xl flex justify-between items-center px-2 mx-auto">
+        <div class="flex items-center">
+          <button class="h-[46px]"><img src="/public/images/viva-logo-white.png" class="h-[32px]"></button>
+          <button class="h-[46px] pl-6"><img src="/public/images/logo-right-gif.png" class="h-[46px]"></button>
+        </div>
+        <div class="flex items-center">
+          <div class="text-white text-lg">購物車</div>
+          <div class="text-white text-lg">會員專區</div>
+          <div class="text-white text-lg">登入</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
