d077cdddd2774bad454dde79e6e25959e76b3abb
HawkingHuang <math0915@gmail.com>
2025-03-14 15:33:01 +0800

    footer 2

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,33 +1,49 @@
 <template>
-  <div class="bg-[#e0dde3] py-12">
-    <div class="container-xl grid grid-cols-4 gap-4">
-      <div class="flex flex-col gap-2">
-        <h6 class="text-black font-black">關於我們</h6>
-        <div>企業資訊</div>
-        <div>廠商合作</div>
-        <div>企業徵才</div>
-        <div>主持群</div>
+  <div>
+    <div class="bg-[#e0dde3] py-12">
+      <div class="container-xl grid grid-cols-4 gap-4">
+        <div class="flex flex-col gap-2">
+          <h6 class="text-black font-black">關於我們</h6>
+          <div>企業資訊</div>
+          <div>廠商合作</div>
+          <div>企業徵才</div>
+          <div>主持群</div>
+        </div>
+        <div class="flex flex-col gap-2">
+          <h6 class="text-black font-black">售後服務</h6>
+          <div>取消/換貨/退貨</div>
+          <div>商品鑑賞期</div>
+          <div>電子發票</div>
+          <div>聯絡我們</div>
+        </div>
+        <div class="flex flex-col gap-2">
+          <h6 class="text-black font-black">常見問題</h6>
+          <div>新手上路</div>
+          <div>購物指南</div>
+          <div>優惠金使用說明</div>
+          <div>會員分級新制辦法</div>
+        </div>
+        <div class="flex flex-col gap-2">
+          <h6 class="text-black font-black">會員權益</h6>
+          <div>隱私權保護政策</div>
+          <div>個資保護專區</div>
+          <div>閱聽人意見服務報告</div>
+          <div></div>
+        </div>
       </div>
-      <div class="flex flex-col gap-2">
-        <h6 class="text-black font-black">售後服務</h6>
-        <div>取消/換貨/退貨</div>
-        <div>商品鑑賞期</div>
-        <div>電子發票</div>
-        <div>聯絡我們</div>
-      </div>
-      <div class="flex flex-col gap-2">
-        <h6 class="text-black font-black">常見問題</h6>
-        <div>新手上路</div>
-        <div>購物指南</div>
-        <div>優惠金使用說明</div>
-        <div>會員分級新制辦法</div>
-      </div>
-      <div class="flex flex-col gap-2">
-        <h6 class="text-black font-black">會員權益</h6>
-        <div>隱私權保護政策</div>
-        <div>個資保護專區</div>
-        <div>閱聽人意見服務報告</div>
-        <div></div>
+    </div>
+    <div class="bg-[#5c5365] py-3">
+      <div class="container-xl">
+        <div class="flex justify-center gap-2 text-white py-1">
+          <div>24H免費服務專線：0809-053-888</div>
+          <div>聯絡信箱：csr.service@vivatv.com.tw</div>
+          <div>地址：臺北市南港區重陽路120號6樓</div>
+        </div>
+        <div class="flex justify-center gap-2 text-white py-1">
+          <div>美好家庭購物股份有限公司版權所有</div>
+          <div>統編：29036132</div>
+          <div>©2024 Shopnet Homeshopping co., Ltd. All Rights Reserved.</div>
+        </div>
       </div>
     </div>
   </div>
