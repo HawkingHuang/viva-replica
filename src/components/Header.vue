e9324e8de91e0c448c5cb576cf5dbafb370f671b
HawkingHuang <math0915@gmail.com>
2025-03-14 12:05:17 +0800

    middle nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,6 +13,41 @@
         </div>
       </div>
     </div>
+
+    <div class="container-xl grid grid-cols-8 py-4 gap-4">
+      <div class="flex flex-col items-center gap-2">
+        <img src="/public/images/F5E2973B615C3EECE05400144FF82309_m.webp" class="w-[132px] rounded-full">
+        旗艦館總覽
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <img src="/public/images/27B6DACBF1D53F30E06400144FF82309_m.jpg" class="w-[132px] rounded-full">
+        悅讀美好
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <img src="/public/images/260FA1A01D375CF8E06400144FF9CEC6_m.jpg" class="w-[132px] rounded-full">
+        本月優惠精選
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <img src="/public/images/F6092A72CB3E2D22E05400144FF82309_m.webp" class="w-[132px] rounded-full">
+        品味生活
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <img src="/public/images/F6092A72CB3F2D22E05400144FF82309_m.webp" class="w-[132px] rounded-full">
+        質感家電
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <img src="/public/images/F6092A72CB402D22E05400144FF82309_m.webp" class="w-[132px] rounded-full">
+        時尚名品
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <img src="/public/images/F6092A72CB5A2D22E05400144FF82309_m.webp" class="w-[132px] rounded-full">
+        健康生活
+      </div>
+      <div class="flex flex-col items-center gap-2">
+        <img src="/public/images/F6092A72CB5B2D22E05400144FF82309_m.webp" class="w-[132px] rounded-full">
+        美味食光
+      </div>
+    </div>
   </div>
 </template>
 
